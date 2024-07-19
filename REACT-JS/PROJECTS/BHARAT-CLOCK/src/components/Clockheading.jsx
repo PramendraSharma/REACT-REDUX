1edd8bf16aaf0c4142163c9cded00e53e8c07b19
Pramendra Sharma <148541679+PramendraSharma@users.noreply.github.com>
2024-07-19 17:25:02 +0530

    Create Clockheading.jsx

---
+++ REACT-JS/PROJECTS/BHARAT-CLOCK/src/components/Clockheading.jsx
@@ -0,0 +1,7 @@
+function ClockHeading(){
+
+    return <h1 className="text-bg-primary me-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center overflow-hidden fw-bolder">BHARAT CLOCK</h1>
+
+}
+
+export default ClockHeading;
