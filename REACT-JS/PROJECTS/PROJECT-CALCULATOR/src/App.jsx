028ae500ef7c654fa0b17e02705edc2d2019cf79
Pramendra Sharma <148541679+PramendraSharma@users.noreply.github.com>
2024-07-19 17:05:51 +0530

    Add files via upload

---
+++ REACT-JS/PROJECTS/PROJECT-CALCULATOR/src/App.jsx
@@ -0,0 +1,36 @@
+// importing stylesheet 
+import styles from "./App.module.css";
+
+// Import useState hook from the react library
+import { useState } from "react";
+
+// Importing components
+import Display from "./components/Display";
+import ButtonsContainer from "./components/ButtonsContainer";
+
+
+function App() {
+  
+  const [calVal, setCalVal] = useState("");
+
+  const onButtonClick = (buttonText) => {
+    if (buttonText === "C") {
+      setCalVal("");
+    } else if (buttonText === "=") {
+      const result = eval(calVal);
+      setCalVal(result);
+    } else {
+      const newDisplayValue = calVal + buttonText;
+      setCalVal(newDisplayValue);
+    }
+  };
+
+  return (
+    <div className={styles.calculator}>
+      <Display displayValue={calVal}></Display>
+      <ButtonsContainer onButtonClick={onButtonClick}></ButtonsContainer>
+    </div>
+  );
+}
+
+export default App;
