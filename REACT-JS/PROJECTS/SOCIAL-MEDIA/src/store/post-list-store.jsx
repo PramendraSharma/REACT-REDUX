1c2bc8cf4cd0b7d4b59e84b198db04bca623ef8d
Pramendra Sharma <148541679+PramendraSharma@users.noreply.github.com>
2024-07-19 17:38:13 +0530

    Update post-list-store.jsx

--- REACT-JS/PROJECTS/SOCIAL-MEDIA/src/store/post-list-store.jsx
+++ REACT-JS/PROJECTS/SOCIAL-MEDIA/src/store/post-list-store.jsx
@@ -56,20 +56,20 @@ const PostListProvider = ({ children }) => {
 
 const DEFAULT_POST_LIST = [
   {
-    id: "1",
-    title: "Going to Mumbai",
-    body: "Hi Friends, I am going to Mumbai for my vacations. Hope to enjoy a lot. Peace out.",
-    reactions: 2,
-    userId: "user-9",
-    tags: ["vacation", "Mumbai", "Enjoying"],
+    id: "001",
+    title: "Going to NEPAL",
+    body: "HELLO THERE I AM GOING TO NEPAL!!!!!!!!",
+    reactions: 50,
+    userId: "user-67",
+    tags: ["vacation", "NEPAL", "Enjoying"],
   },
   {
-    id: "2",
-    title: "Paas ho bhai",
-    body: "4 saal ki masti k baad bhi ho gaye hain paas. Hard to believe.",
+    id: "006",
+    title: "CASUAL",
+    body: "REACT IS VERY EASY....",
     reactions: 15,
     userId: "user-12",
-    tags: ["Graduating", "Unbelievable"],
+    tags: ["REACT", "EASY"],
   },
 ];
 
