33b9db9f0ba0f4cc861cac3823ab296402694776
PRO_STUDIO_PRODUCTIONS <148541679+PramendraSharma@users.noreply.github.com>
2024-07-24 17:03:59 +0530

    Create Contact.jsx

---
+++ REACT-JS/PROJECTS/PORTFOLIO-WEBPAGE/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to contact me!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="pramendra.s.production@gmail.com">pramendra.s.production@gmail.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="www.linkedin.com/in/pramendra-sharma-027a13298">Linkedin.com/Pramendra_Sharma</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://github.com/PramendraSharma">Github.com/Pramendra_Sharma</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
