d07d8e79ee3b179d4d91aaa666ce6c8291da0723
PRO_STUDIO_PRODUCTIONS <148541679+PramendraSharma@users.noreply.github.com>
2024-07-24 17:02:42 +0530

    Create About.jsx

---
+++ REACT-JS/PROJECTS/PORTFOLIO-WEBPAGE/src/components/about/About.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+import styles from "./About.module.css";
+import { getImageUrl } from "../../utils";
+
+export const About = () => {
+  return (
+    <section className={styles.container} id="about">
+      <h2 className={styles.title}>About</h2>
+      <div className={styles.content}>
+        {/* <img
+          src={getImageUrl("about/aboutImage.png")}
+          alt="NewImage"
+          className={styles.aboutImage}
+        /> */}
+        <ul className={styles.aboutItems}>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Frontend Developer</h3>
+              <p>
+                I'm a frontend developer with experience in building responsive
+                and optimized sites. 
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="Cursor icon" />
+            <div className={styles.aboutItemText}>
+              <h3>UI Designer</h3>
+              <p>
+                I have designed multiple landing pages and helped in changing the design of multiple components of a webpage
+              </p>
+            </div>
+          </li>
+          <li className={styles.aboutItem}>
+            <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+            <div className={styles.aboutItemText}>
+              <h3>Data Analyst</h3>
+              <p>
+                Sorted and filtered test data to derive important  data.
+              </p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+};
